8f1f6037da913f49d40c4e013f944c6eea12920b
Catherine <katrinker@bk.ru>
2023-12-14 22:51:29 +0700

    do 3 task

--- Program.cs
+++ Program.cs
@@ -58,7 +58,7 @@ FindEl();*/
 //Задача 2: Задайте двумерный массив. 
 //Напишите программу, которая поменяет местами первую и последнюю строку массива.
 
-int [,] array = new int[4,4];
+/*int [,] array = new int[5,4];
 
 void CreateArray()
 {
@@ -88,8 +88,8 @@ void ChangeArray()
 {
    for(int i = 0; i < array.GetLength(1); i++)
             {
-                var tmp = array[3, i];
-                array[3, i] = array[0, i];
+                var tmp = array[array.GetLength(0)-1, i];
+                array[array.GetLength(0)-1, i] = array[0, i];
                 array[0, i] = tmp;
             }
 System.Console.WriteLine();
@@ -99,4 +99,63 @@ System.Console.WriteLine();
 CreateArray();
 PrintArray();
 ChangeArray();
+PrintArray();*/
+
+
+
+
+//Задача 3: Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+/*int [,] array = new int[6,5];
+
+void CreateArray()
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+array[i,j] = new Random().Next(1,10);
+}
+}
+}
+
+void PrintArray()
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+System.Console.Write(array[i,j] +" ");
+}
+System.Console.WriteLine();
+}
+}
+
+void FindStringMinSum()
+{
+int[] sum = new int[array.GetLength(0)];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+sum[i]+=array[i,j];
+}
+}
+int minI = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+            {
+                if (sum[minI] > sum[i]) minI = i;
+            }
+            Console.Write($"\nНаименьшая сумма элементов: {sum[minI]}, номер строки с ней: {minI}");
+
+}
+
+CreateArray();
 PrintArray();
+FindStringMinSum();*/
+
+
+
+
